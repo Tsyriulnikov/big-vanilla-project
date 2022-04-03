7950f33669bcbc17f6bf76ed06f2c1f744b49dda
Tsyriulnikov <max_djvu@mail.ru>
2022-04-03 11:12:21 +0300

    issue002_2

--- src/Codewars/issue002.test.ts
+++ src/Codewars/issue002.test.ts
@@ -1,21 +1,48 @@
-const palindrome = (num:number) : number|string=> {
-let reverseNum:number
-let stringNum:string = num.toString()
-let polNum:number= num
+const palindrome = (num: any): number | string => {
+    let reverseNumUp: number = Number(num.toString().split('').reverse().join(''));
+    let reverseNumDown: number = Number(num.toString().split('').reverse().join(''));
+    let polNum: number = num
+    let result: number | "Not valid" = 11
 
+    if ( num > 11 && Number.isInteger(num)) {
+        while (reverseNumUp !== polNum) {
+            ++polNum
+            reverseNumUp = Number(polNum.toString().split('').reverse().join(''))
+        }
+        polNum= num
+        while (reverseNumDown !== polNum) {
+            --polNum
+            reverseNumDown = Number(polNum.toString().split('').reverse().join(''))
+        }
+        ;
+        (num - reverseNumDown < reverseNumUp - num) ? result = reverseNumDown : result = reverseNumUp
+    };
+    if (!Number.isInteger(num) || num < 0)  result = "Not valid"
 
-//    while (reverseNum !== num ) {
-         polNum++
-         reverseNum = Number(polNum.toString().split('').reverse().join(''))
-
-  //   }
-
-    return reverseNum
+    return result
 
 }
-
 
 
 test('Numerical Palindrome #4', () => {
     expect(palindrome(281)).toBe(282);
 });
+test('Numerical Palindrome #4.1', () => {
+    expect(palindrome(8)).toBe(11);
+});
+test('Numerical Palindrome #4.2', () => {
+    expect(palindrome("12345")).toBe("Not valid");
+});
+
+//
+
+// function isPalindrome(num){
+// //     return (num>10 && num == num.toString().split("").reverse().join(""))? true : false;
+// // }
+// // function palindrome(num) {
+// //     if(!Number.isInteger(num) || num<0) return 'Not valid';
+// //     for(i=0; 1==1; i++){
+// //         if(isPalindrome(num+i)) return num+i;
+// //         if(isPalindrome(num-i)) return num-i;
+// //     }
+// }
